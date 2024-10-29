617d3004942cf9d616181486036eeb3fda142236
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-10-29 21:14:24 +0800

    added api call to console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+async function Weather() {
+    const response = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/singapore?unitGroup=metric&include=current&key=4776LACF5YLTJYS6JKTPBCHG9&contentType=json")
+    const json = await response.json()
+    console.log(json)
+}
+
+Weather()
